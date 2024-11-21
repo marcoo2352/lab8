e46bc5296470f060f4248a1960980563ee61c3a2
Marco Gallo <mgallo811@gmail.com>
2024-11-21 16:05:51 +0100

    cambiamenti

---
+++ lab8.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import altair as alt
+import polars as pl
+
+
+data_url = "https://www.dei.unipd.it/~ceccarello/data/gapminder.csv"
+data = pl.read_csv(data_url).filter(pl.col("year") == 2007)
+datac = pl.read_csv(data_url)
+st.write(datac)
+
+st.write("# A bubble chart")
+
+chart = (
+    alt.Chart(data)
+    .mark_circle()
+    .encode(
+        x=alt.X("gdpPercap").scale(type="log"),
+        y=alt.Y("lifeExp").scale(zero=False),
+        size=alt.Size("pop"),
+        color=alt.Color("continent")
+    )
+)
+
+st.altair_chart(chart, use_container_width=True)
+
+st.write("# A bar chart, with aggregations")
+st.altair_chart(
+    alt.Chart(data)
+    .mark_bar()
+    .encode(
+        alt.Y("continent", sort="x"),
+        alt.X("sum(pop)")
+    ),
+    use_container_width=True
+)
+
+st.write("Relation between years and life expextancy")
+st.altair_chart(
+    alt.Chart(data).mark_point().properties(height = 500, width= 800).encode(
+        alt.X("lifeExp:Q").scale(zero=False),
+        alt.Y("continent:O"),
+        color=alt.Color("continent:O", scale=alt.Scale(scheme="category10"))
+))
+
+st.altair_chart(
+    alt.Chart(datac).mark_line().properties(height = 500, width= 800).encode(
+        alt.Y("lifeExp:Q", aggregate = "median" ).scale(zero=False), #aggregate
+        alt.X("year:O"),
+        color=alt.Color("continent:O", scale=alt.Scale(scheme="category10"))        
+    )
+)
+# Dizionario che mappa ogni continente a un colore specifico
+color_mapping = {
+    'Asia': 'yellow',
+    'Europe': 'blue',
+    'Africa': 'green',
+    'Americas': 'purple',
+    'Oceania': 'red'
+}
+
+
+
+st.altair_chart(
+    alt.Chart(data).mark_arc(radius=90, radius2=100).encode(
+        theta="sum(pop)",
+       color=alt.Color("continent:O", scale=alt.Scale(domain=list(color_mapping.keys()), range=list(color_mapping.values())))
+    )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
